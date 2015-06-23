e45875cfbf5ab84a33abeb994eabc552bc6f9407
qiankaijie <qiankaijie@126.com>
2015-06-23 15:31:19 +0800

    shear ok

--- src/js/index.js
+++ src/js/index.js
@@ -18,7 +18,68 @@ $(document).ready(function() {
 
     //初始化点击率
     $.get("/marketingactivity/f89aa7644e1ea45e014e1eff343c12b6/clickno", function(no) {});
-   
+
+    var curl = window.location.href;
+
+    var url = "/wxpay/prepare";
+    $.ajax({
+        type: "post",
+        url: url,
+        data: {
+            "curl": curl
+        },
+        success: function(response) {
+            wx.config({
+                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                appId: response['appid'], // 必填，公众号的唯一标识
+                timestamp: response['timestamp'], // 必填，生成签名的时间戳
+                nonceStr: response['noncestr'], // 必填，生成签名的随机串
+                signature: response['sign_result'], // 必填，签名，见附录1
+                jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage']
+            });
+        },
+        error: function(response) {
+            console.log("ERROR:", response)
+        }
+    });
+
+    wx.ready(function() {
+        wx.onMenuShareTimeline({
+            title: '微信时光机标题', // 分享标题
+            link: 'http://whiletime.com/timemachineDist/html/timemachine.html?openId='+RequestParameter()['openId'], // 分享链接
+            imgUrl: 'http://whiletime.com/timemachineDist/img/title.png', // 分享图标
+            success: function() {
+                // 用户确认分享后执行的回调函数
+                $.get("/marketingactivity/f89aa7644e1ea45e014e1eff343c12b6/addshare");
+            },
+            cancel: function() {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+
+        wx.onMenuShareAppMessage({
+            title: '时光机时光机标题', // 分享标题
+            desc: '时光机描述', // 分享描述
+            link: 'http://whiletime.com/timemachineDist/html/timemachine.html?openId='+RequestParameter()['openId'], // 分享链接
+            imgUrl: 'http://whiletime.com/timemachineDist/img/title.png', // 分享图标
+            success: function() {
+                // 用户确认分享后执行的回调函数
+                $.get("/marketingactivity/f89aa7644e1ea45e014e1eff343c12b6/addshare");
+
+            },
+            cancel: function() {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+
+        // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+    });
+
+
+
+    wx.error(function(res) {
+        console.log(res);
+    });
 
 
 
